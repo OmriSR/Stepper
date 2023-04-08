aeda5c8da96019b2cf0b1ab3ff76af9c270f6c69
omriSR <omri.1996s@gmail.com>
2023-04-08 10:53:40 +0300

    finished relation system data. it has 3 types of ctors: 1 gets the SD ready for work(as a map) 2 get only the titles and creates a map with keys but no values (empty lists) 3 gets titles and values separately and creats a map
    all ctors update the num of row data member

--- engine/src/Main.java
+++ engine/src/Main.java
@@ -1,5 +1,41 @@
+import systemdata.Relation;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List titles = new ArrayList<String>();
+        List values = new ArrayList<List>();
+        List val1 = new ArrayList<>();
+        List val2 = new ArrayList<>();
+        List val3 = new ArrayList<>();
+
+        val1.add("Hello");
+        val1.add("world");
+
+        val2.add("Tomer");
+        val2.add("Gay");
+
+        val3.add("Fortnite");
+
+        values.add(val1);
+        values.add(val2);
+        values.add(val3);
+
+        titles.add("first");
+        titles.add("second");
+        titles.add("third and last");
+        Relation rel = new Relation(titles);
+        Relation rel2 = new Relation(titles,values);
+
+        out.println("this is a test of the second ctor:");
+        rel.presentToUser();
+        out.println("\n");
+        out.println("this is a test of the third ctor");
+        rel2.presentToUser();
+
     }
 }
--- engine/src/systemdata/Relation.java
+++ engine/src/systemdata/Relation.java
@@ -1,23 +1,24 @@
 package systemdata;
 import engine.SystemData;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
+
+import static java.lang.System.out;
 
 public class Relation extends SystemData<Map<String,List<Object>>> {
     private Integer numOfRows = 0;
+
 //    This ctor receives:
 //      * a ready-made table
-    protected Relation(Map<String,List<Object>> content) {
+    public Relation(Map<String,List<Object>> content) {
         super("relation", false, content);
     }
 //    This ctor receives:
 //      * a list of string -> titles of the columns in the table (no data = 0 rows)
-    protected Relation(List<String> tableTitles) {
+    public Relation(List<String> tableTitles) {
         super("relation", false, null);
 
-        Map<String,List<Object>> content = new HashMap<>();
+        Map<String,List<Object>> content = new LinkedHashMap<>();
         for(int i=0; i< tableTitles.size();++i){
             content.put(tableTitles.get(i), new ArrayList<>());
         }
@@ -26,11 +27,11 @@ public class Relation extends SystemData<Map<String,List<Object>>> {
     }
 //    This ctor receives:
 //      * a list of string (title of the columns in the table)
-//        * a list of (list of) objects -> every item in the list is data of that column
-    protected Relation(List<String> tableTitles, List<List<Object>> tableData) {
+//      * a list of (list of) objects -> every item in the list is data of that column
+    public Relation(List<String> tableTitles, List<List<Object>> tableData) {
         super("relation", false, null);
 
-        Map<String,List<Object>> content = new HashMap<>();
+        Map<String,List<Object>> content = new LinkedHashMap<>();
         for(int i=0; i< tableTitles.size();++i){
             content.put(tableTitles.get(i), tableData.get(i));
         }
@@ -41,7 +42,11 @@ public class Relation extends SystemData<Map<String,List<Object>>> {
 
     @Override
     public void presentToUser() {
+        out.println("There are " + this.numOfRows + " rows in the " + this.name);
+        out.println("The column titles are: " + this.content.keySet().toString());
 
+        //testing
+//        out.println("the values of the relation are:" + this.content.values().toString());
     }
 
     public Integer getNumOfRows() {
